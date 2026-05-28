955d7f72543ccf4d013db93d7394ed1e90b5e531
Nina Aksenich <maruaninashved1488@gmail.com>
2026-05-28 15:08:47 +0300

    fix mongodb timeout

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -3,11 +3,12 @@ import mongoose from 'mongoose';
 export const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
-      serverSelectionTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 30000,
     });
 
     console.log('✅ MongoDB connection established successfully');
   } catch (error) {
-    console.log(error);
+    console.log('❌ Mongo connection error:', error);
+    process.exit(1);
   }
 };
